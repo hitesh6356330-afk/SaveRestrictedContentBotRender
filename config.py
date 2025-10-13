4c62c09ec67367ee64bf69099f9a196c666b8afd
clashsmart1256 <75825860+clashsmart1256@users.noreply.github.com>
2025-10-13 20:55:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+IG_USERNAME = "your_instagram_username"
+IG_PASSWORD = "your_instagram_password"
 # Fill with your data
 BOT_TOKEN = "8263131820:AAGpoyAtisEpNKTkNMeA4g6Jq7hvKyz-Zz0"
 DATA_GROUP_ID = -1003160468688  # Replace with your group ID where data is stored
