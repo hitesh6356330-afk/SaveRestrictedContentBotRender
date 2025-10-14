be7f2d352fc923b66972cb57f2b6b0015aa69a43
clashsmart1256 <75825860+clashsmart1256@users.noreply.github.com>
2025-10-14 20:55:25 +0530

    Update and rename config.py to render.yaml

--- config.py
+++
@@ -1,4 +0,0 @@
-# Fill with your data
-BOT_TOKEN = "8263131820:AAGpoyAtisEpNKTkNMeA4g6Jq7hvKyz-Zz0"
-DATA_GROUP_ID = -1003160468688  # Replace with your group ID where data is stored
-BOT_OWNER_ID = 5451324394        # your telegram user id
